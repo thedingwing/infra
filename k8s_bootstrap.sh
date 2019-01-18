e499d14f3377f17d251f61896fa7726e5b2f3057
Seth Itow <seth@Seths-MacBook-Pro.local>
2019-01-17 22:57:57 -0800

    Initial commit.

---
+++ k8s_bootstrap.sh
@@ -0,0 +1,8 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+helm install --name cert-manager --namespace kube-system stable/cert-manager
+kubectl apply -f staging_issuer.yaml
